18eab470ade50a2f6bd7e1f079479ba8221b0cd6
Eshan Amanda <eshanesn97@gmail.com>
2022-06-03 05:13:43 +0530

    Add working files

---
+++ app/src/main/java/com/example/datastructuresproject/FragmentAdapter.java
@@ -0,0 +1,30 @@
+package com.example.datastructuresproject;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+        public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position){
+                case 1:
+                    return new ArrayWise();
+            }
+            return new NumberWise();
+        }
+
+        @Override
+        public int getItemCount() {
+            return 2;
+        }
+    }
+
+
--- app/src/main/java/com/example/datastructuresproject/MainActivity.java
+++ app/src/main/java/com/example/datastructuresproject/MainActivity.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
     Button b_tree, bckt_sort;
+    ImageView pattern;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         b_tree = findViewById(R.id.btree);
         bckt_sort = findViewById(R.id.bucket);
+        pattern = findViewById(R.id.pat);
+
+        Glide.with(this).load(R.drawable.data).into(pattern);
+
 
         b_tree.setOnClickListener(new View.OnClickListener() {
             @Override
