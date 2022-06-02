458cf18446dfc350539d7851d2c615ea2d12bb1b
Eshan Amanda <eshanesn97@gmail.com>
2022-06-03 05:07:56 +0530

    Add main UIs to project

---
+++ app/src/main/java/com/example/datastructuresproject/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.example.datastructuresproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        ImageView penguin = (ImageView) findViewById(R.id.penguin);
+
+        Glide.with(this).load(R.drawable.penguin).into(penguin);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
+    }
+}
